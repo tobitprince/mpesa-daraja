f2055a77d82dcdf92873f2542b42d91c79864647
tobitprince <prince.tobit@strathmore.edu>
2023-09-06 14:32:22 +0300

    minor changes

--- callback_url.php
+++ callback_url.php
@@ -10,7 +10,7 @@
      $mpesaResponse = file_get_contents('php://input');
  
      // log the response
-     $logFile = "M_PESAConfirmationResponse.txt";
+     $logFile = "M_PESAConfirmationResponse.json";
  
      // write to file
      $log = fopen($logFile, "a");
@@ -18,4 +18,28 @@
      fwrite($log, $mpesaResponse);
      fclose($log);
  
+     //processing the mpesa json response data
+     $mpesaResponse = file_get_contents('M_PESAConfirmationResponse.json');
+     $callbackContent = json_decode($mpesaResponse);
+
+     $Resultcode = $callbackContent->Body->stkCallback->ResultCode;
+     $CheckoutRequestID = $callbackContent->Body->stkCallback->CheckoutRequestID;
+     $Amount = $callbackContent->Body->stkCallback->CallbackMetadata->Item[0]->Value;
+     $MpesaReceiptNumber = $callbackContent->Body->stkCallback->CallbackMetadata->Item[1]->Value;
+     $PhoneNumber-> $callbackContent->Body->stkCallback->CallbackMetadata->Item[4]->Value;
+     $formatedPhone = str_replace("254", "0", $PhoneNumber);
+     if ($Resultcode == 0){
+
+      //connect to DB
+      $conn = mysqli_connect("localhost", "root", "", "mpesa");
+      if($conn->connect_error) {
+        die("<h1>Connection failed:<h1> " . $conn->connect_error);
+      }else {
+
+        $insert = $conn->query("insert into data(CheckoutRequestID,Resultcode,Amount,MpesaReceiptNumber,PhoneNumber)");
+        $conn = null;
+      }
+     }
+
+
      echo $response;
