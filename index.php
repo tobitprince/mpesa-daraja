cd083fd3d33a36d359625c92d4e7104c4041a7d6
tobitprince <prince.tobit@strathmore.edu>
2023-09-07 02:09:19 +0300

    changed names

--- index.php
+++ index.php
@@ -1,118 +1,214 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" >
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lipa na mpesa</title>
-    <link
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
-    <link href="" rel="stylesheet" />
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" ">
-    <script
-      type="text/javascript"
-      src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"
-    ></script>
-    <style>
-      @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap");
+  <meta charset="UTF-8">
+  <title>Church Form</title>
+  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'><link rel="stylesheet" href="./style.css">
 
-      body {
-        background-color: #eaedf4;
-        font-family: "Rubik", sans-serif;
-      }
-
-      .card {
-        width: 310px;
-        border: none;
-        border-radius: 15px;
-      }
-
-      .justify-content-around div {
-        border: none;
-        border-radius: 20px;
-        background: #f3f4f6;
-        padding: 5px 20px 5px;
-        color: #8d9297;
-      }
-
-      .justify-content-around span {
-        font-size: 12px;
-      }
-
-      .justify-content-around div:hover {
-        background: #545ebd;
-        color: #fff;
-        cursor: pointer;
-      }
-
-      .justify-content-around div:nth-child(1) {
-        background: #545ebd;
-        color: #fff;
-      }
-
-      span.mt-0 {
-        color: #8d9297;
-        font-size: 12px;
-      }
-
-      h6 {
-        font-size: 15px;
-      }
-      .mpesa {
-        background-color: green !important;
-      }
-
-      img {
-        border-radius: 15px;
-      }
-    </style>
-  </head>
-  <body oncontextmenu="return false" class="snippet-body">
-    <div class="container d-flex justify-content-center">
-      <div class="card mt-5 px-3 py-4">
-        <div class="d-flex flex-row justify-content-around">
-          <div class="mpesa"><span>Mpesa </span></div>
-          <div><span>Paypal</span></div>
-          <div><span>Card</span></div>
-        </div>
-        <div class="media mt-4 pl-2">
-          <img src="./images/1200px-M-PESA_LOGO-01.svg.png" class="mr-3" height="75" />
-          <div class="media-body">
-            <h6 class="mt-1">Enter Amount & Number</h6>
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="image" >
+    
+</div>
+<div class="container1">
+    <p><strong>WITH DR. RODNEY HOWARD-BROWNE.</strong><br>
+NEW CONVERTS REGISTRATION FORM.</p>
+</div>
+<div class="container">
+  <form>
+    <div class="row">
+      <h4>Personal Details</h4>
+      <div class="input-group input-group-icon">
+        <input type="text" placeholder="Full Name"/>
+        <div class="input-icon"><i class="fa fa-user"></i></div>
+      </div>
+      <div class="input-group input-group-icon">
+        <input type="email" placeholder="Email Adress"/>
+        <div class="input-icon"><i class="fa fa-envelope"></i></div>
+      </div>
+      <div class="input-group input-group-icon">
+        <input type="number" placeholder="Phone Number"/>
+        <div class="input-icon"><i class="fa fa-phone-square"></i></div>
+      </div>
+      <div class="input-group input-group-icon">
+        <input type="number" placeholder="Alternative Phone Number"/>
+        <div class="input-icon"><i class="fa fa-phone-square"></i></div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-half">
+        <h4>Age Group</h4>
+        <div class="input-group">
+          <div class="col-half">
+            <select name="age-group" id="age-group" style="width:100%"!important;>
+            <option>18 - 29</option>
+            <option>30 - 39</option>
+            <option>40 - 49</option>
+            <option>50 - 59</option>
+            <option>60 - 69</option>
+            <option>70 - 79</option>
+            <option>80+</option>
+          </select>
           </div>
+          
         </div>
-        <div class="media mt-3 pl-2">
-                          <!--bs5 input-->
-
-            <form class="row g-3" action="./stk_initiate.php" method="POST">
-            
-                <div class="col-12">
-                  <label for="inputAddress" class="form-label">Amount</label>
-                  <input type="text" class="form-control" name="amount" placeholder="Enter Amount">
-                </div>
-                <div class="col-12">
-                  <label for="inputAddress2" class="form-label" >Phone Number</label>
-                  <input type="text" class="form-control" name="phone"  placeholder="Enter Phone Number">
-                </div>
-             
-                <div class="col-12">
-                  <button type="submit" class="btn btn-success" name="submit" value="submit">Push</button>
-                </div>
-              </form>
-              <!--bs5 input-->
-          </div>
+      </div>
+      <div class="col-half">
+        <h4>Gender</h4>
+        <div class="input-group">
+          <input id="gender-male" type="radio" name="gender" value="male"/>
+          <label for="gender-male">Male</label>
+          <input id="gender-female" type="radio" name="gender" value="female"/>
+          <label for="gender-female">Female</label>
         </div>
       </div>
     </div>
-    <script
-      type="text/javascript"
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
-    ></script>
-    <script type="text/javascript" src=""></script>
-    <script type="text/javascript" src=""></script>
-    <script type="text/Javascript"></script>
-  </body>
+    <div class="row">
+      <h4>Profession</h4>
+      <div class="input-group input-group-icon">
+        <input type="text" placeholder="Profession"/>
+        <div class="input-icon"><i class="fa fa-briefcase "></i></div>
+      </div>
+      </div>
+      <div class="row">
+      <div class="col-half">
+        <h4>Religious Background</h4>
+        <div class="input-group">
+          <div class="col-half">
+            <select name="age-group" id="age-group" style="width:100%"!important;>
+            <option>Pentecostal</option>
+            <option>Evangelical</option>
+            <option>Protestant</option>
+            <option>Catholic</option>
+            <option>Indigenous</option>
+            <option>Muslim</option>
+            <option>Buddhist</option>
+            <option>Atheist</option>
+          </select>
+          </div>
+          
+        </div>
+      </div>
+      </div>
+      <div class="row">
+      <div class="col-half">
+        <h4>County of Residence</h4>
+        <div class="input-group">
+          <div class="col-half">
+            <select name="age-group" id="age-group" style="width:100%"!important;>
+            <option value="baringo">Baringo</option>
+            <option value="bomet">Bomet</option>
+            <option value="bungoma">Bungoma</option>
+            <option value="busia">Busia</option>
+            <option value="elgeyo marakwet">Elgeyo Marakwet</option>
+            <option value="embu">Embu</option>
+            <option value="garissa">Garissa</option>
+            <option value="homa bay">Homa Bay</option>
+            <option value="isiolo">Isiolo</option>
+            <option value="kajiado">Kajiado</option>
+            <option value="kakamega">Kakamega</option>
+            <option value="kericho">Kericho</option>
+            <option value="kiambu">Kiambu</option>
+            <option value="kilifi">Kilifi</option>
+            <option value="kirinyaga">Kirinyaga</option>
+            <option value="kisii">Kisii</option>
+            <option value="kisumu">Kisumu</option>
+            <option value="kitui">Kitui</option>
+            <option value="kwale">Kwale</option>
+            <option value="laikipia">Laikipia</option>
+            <option value="lamu">Lamu</option>
+            <option value="machakos">Machakos</option>
+            <option value="makueni">Makueni</option>
+            <option value="mandera">Mandera</option>
+            <option value="meru">Meru</option>
+            <option value="migori">Migori</option>
+            <option value="marsabit">Marsabit</option>
+            <option value="mombasa">Mombasa</option>
+            <option value="muranga">Muranga</option>
+            <option value="nairobi">Nairobi</option>
+            <option value="nakuru">Nakuru</option>
+            <option value="nandi">Nandi</option>
+            <option value="narok">Narok</option>
+            <option value="nyamira">Nyamira</option>
+            <option value="nyandarua">Nyandarua</option>
+            <option value="nyeri">Nyeri</option>
+            <option value="samburu">Samburu</option>
+            <option value="siaya">Siaya</option>
+            <option value="taita taveta">Taita Taveta</option>
+            <option value="tana river">Tana River</option>
+            <option value="tharaka nithi">Tharaka Nithi</option>
+            <option value="trans nzoia">Trans Nzoia</option>
+            <option value="turkana">Turkana</option>
+            <option value="uasin gishu">Uasin Gishu</option>
+            <option value="vihiga">Vihiga</option>
+            <option value="wajir">Wajir</option>
+            <option value="pokot">West Pokot</option>
+          </select>
+          </div>
+          
+        </div>
+      </div>
+      <div class="col-half">
+        <h4>Subcounty of Residence</h4>
+        <div class="input-group">
+          <div class="col-half">
+            <select name="age-group" id="age-group" style="width:100%"!important;>
+            <option>Ainamoi</option>
+            <option>Baringo North</option>
+            <option>Baringo South</option>
+            <option>Belgut</option>
+            <option>Langata</option>
+            <option>Nairobi West</option>
+            <option>Eldas</option>
+          </select>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <h4>Payment Details</h4>
+      <div class="input-group">
+        <input id="payment-method-card" type="radio" name="payment-method" value="card" checked="true"/>
+        <label for="payment-method-card"><span><i class="fa fa-cc-visa"></i>Credit Card</span></label>
+        <input id="payment-method-paypal" type="radio" name="payment-method" value="paypal"/>
+        <label for="payment-method-paypal"> <span><i class="fa fa-cc-paypal"></i>Paypal</span></label>
+      </div>
+      <div class="input-group input-group-icon">
+        <input type="text" placeholder="Card Number"/>
+        <div class="input-icon"><i class="fa fa-credit-card"></i></div>
+      </div>
+      <div class="col-half">
+        <div class="input-group input-group-icon">
+          <input type="text" placeholder="Card CVC"/>
+          <div class="input-icon"><i class="fa fa-user"></i></div>
+        </div>
+      </div>
+      <div class="col-half">
+        <div class="input-group">
+          <select>
+            <option>01 Jan</option>
+            <option>02 Jan</option>
+          </select>
+          <select>
+            <option>2015</option>
+            <option>2016</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <h4>Terms and Conditions</h4>
+      <div class="input-group">
+        <input id="terms" type="checkbox"/>
+        <label for="terms">I accept the terms and conditions for signing up to this service, and hereby confirm I have read the privacy policy.</label>
+      </div>
+    </div>
+  </form>
+</div>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
+
+</body>
 </html>
